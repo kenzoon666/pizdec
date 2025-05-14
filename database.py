0aeca44aeda2ac2123e46f7ee3ebbea592e1a7ba
kenzoon666 <prost_akk@list.ru>
2025-05-14 14:28:23 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+Base = declarative_base()
+
+class Flashcard(Base):
+    __tablename__ = 'flashcards'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    word = Column(String)
+    translation = Column(String)
+    next_review = Column(Date)  # Для SRS
+
+engine = create_engine('sqlite:///bot.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
