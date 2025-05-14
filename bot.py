d0721c689da334635b170f50da4ae953c86b0298
kenzoon666 <prost_akk@list.ru>
2025-05-14 14:28:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+bot = Bot(token="YOUR_BOT_TOKEN")
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.reply("Привет! Я помогу тебе учить слова. Добавь новое слово: /add")
+
+# Запуск бота
+if __name__ == '__main__':
+    from aiogram import executor
+    executor.start_polling(dp)
